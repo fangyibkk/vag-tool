40bbd09bb5ad01c792ba1a69e976c2c91a548aaa
fangyibkk <fangyibkk@gmail.com>
2016-09-11 09:23:53 +0700

    add remove_spike script

--- src/script_dfa.sh
+++ src/script_dfa.sh
@@ -6,23 +6,27 @@ IFS=$'\n\t'
 # on directory that contain subdirectory /imfs
 # then write to subdirectory \texttt{/dfa\_pair}
 
-in_dir=$1
-out_dir=$2
+parent_in_dir=$1/
+echo "===> Your input parent directory is: $1"
+parent_out_dir=$2/
+echo "===> Your output parent directory is: $2"
 
 for d in $in_dir; do
     # echo $f
     # echo "$f.out"
-    echo "===== working on $d ====="
-    out_dir=$(echo $d | sed "s/imfs/dfa_pair/g")
+    echo "===> working on $d"
     for f in $d/*; do
         echo "process imf $f"
-        out_name=$(echo "$f" | sed "s/imfs/dfa_pair/g")
+	out_name=$parent_out_dir$d/$f
 
         # WARNING: hard code
         # create dir if not exists
-        [ -d $out_dir ] || mkdir -p $out_dir
+        # [ -d $out_dir ] || mkdir -p $out_dir
 
         echo "write to $out_name"
-        ./dfa < $f > $out_name
+        # ./dfa < $f > $out_name
     done
 done
+
+# out_dir=$(echo $d | sed "s/imfs/dfa_pair/g")
+# out_name=$(echo "$f" | sed "s/imfs/dfa_pair/g")
