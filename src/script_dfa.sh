61262ada25ab2f928a9746e762bdd42d8963f29b
fangyibkk <fangyibkk@gmail.com>
2016-09-11 09:50:55 +0700

    rewrite script_dfa

--- src/script_dfa.sh
+++ src/script_dfa.sh
@@ -11,22 +11,16 @@ echo "===> Your input parent directory is: $1"
 parent_out_dir=$2/
 echo "===> Your output parent directory is: $2"
 
-for d in $in_dir; do
-    # echo $f
-    # echo "$f.out"
-    echo "===> working on $d"
+for d in $parent_in_dir/*; do
+    echo "===> working on subdirectory $d"
     for f in $d/*; do
-        echo "process imf $f"
-	out_name=$parent_out_dir$d/$f
+        echo "process file in the subdirectory: $f"
+	out_dir=$parent_out_dir$(echo $d | sed "s;$parent_in_dir;;")
+	out_name=$parent_out_dir$(echo $f | sed "s;$parent_in_dir;;")
+	# create dir if not exists
+	[ -d $out_dir ] || mkdir -p $out_dir
 
-        # WARNING: hard code
-        # create dir if not exists
-        # [ -d $out_dir ] || mkdir -p $out_dir
-
-        echo "write to $out_name"
-        # ./dfa < $f > $out_name
+	echo "write to $out_name"
+        dfa < $f > $out_name
     done
 done
-
-# out_dir=$(echo $d | sed "s/imfs/dfa_pair/g")
-# out_name=$(echo "$f" | sed "s/imfs/dfa_pair/g")
